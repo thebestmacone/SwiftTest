c79e4bf8d0dab4f57698cd4e055c40235a36b3b2
Macone <home@mac-mini-macone.local>
2016-02-28 15:02:47 +0200

    add Meal class model and UnitTests for it

--- SwiftTest/Meal.swift
+++ SwiftTest/Meal.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-//class Meal {
-//    // MARK: Properties
-//    
-//    var name: String
-//    var photo: UIImage?
-//    var rating: Int
-//    
-//    // MARK: Initialization
-//    
-//    init(name: String, photo: UIImage?, rating: Int) {
-//        
-//    }
-//}
+class Meal {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        if name.isEmpty || rating < 0
+        {
+            return nil
+        }
+    }
+}
---
+++ SwiftTest/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  SwiftTest
+//
+//  Created by Macone on 21.02.16.
+//  Copyright © 2016 Macone. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
